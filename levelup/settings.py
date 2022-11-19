c49b2c3e53586eccf054644b400374c39b182236
Sariah Campopiano <scampopiano925@gmail.com>
2022-11-19 10:32:58 -0600

    Updated auth.py code to fit our project and avoid errors. Also updated settings.py

--- levelup/settings.py
+++ levelup/settings.py
@@ -43,12 +43,8 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-    ),
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': [],
 }
 
 CORS_ORIGIN_WHITELIST = (
