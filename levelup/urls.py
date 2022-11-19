08c17452340baae87533e38bef88848176964ecd
Sariah Campopiano <scampopiano925@gmail.com>
2022-11-19 12:15:48 -0600

    Added the url for gametypes in urls.py

--- levelup/urls.py
+++ levelup/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-from levelupapi.views import register_user, check_user
+from levelupapi.views import register_user, check_user, GameTypeView
+from django.conf.urls import include
+from rest_framework import routers
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'gametypes', GameTypeView, 'gametype')
 
 urlpatterns = [
     path('register', register_user),
     path('checkuser', check_user),
     path('admin/', admin.site.urls),
+    path('', include(router.urls))
 ]
